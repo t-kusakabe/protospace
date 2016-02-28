83a9d4e8c9c01e336929d54afccbe8685c0e985f
t-kusakabe <ttmac0704@gmail.com>
2016-02-28 17:41:09 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/:id' => 'users#show'
   root 'top#index'
-  resources :proto, only: [:show, :new, :edit, :update, :destroy]
-  post 'prototypes' => 'proto#create'
+  resources :users, only: [:show]
+  resources :prototypes, except: [:index]
 end
