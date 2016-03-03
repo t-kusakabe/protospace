26bc4b6ebd63a371a7a7dbd8ab32c8b06070cba8
t-kusakabe <ttmac0704@gmail.com>
2016-03-03 17:01:06 +0900

    dependent delete追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :delete_all
   has_many :thumbnails
   accepts_nested_attributes_for :thumbnails, reject_if: proc { |attributes| attributes[:thumbnail].blank? }
 
