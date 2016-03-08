1c315babece26e1f874f6b7b721a9e010d01a46c
t-kusakabe <ttmac0704@gmail.com>
2016-03-08 19:09:11 +0900

    インスタンス変数の生成をメソッド化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,16 @@
 class LikesController < ApplicationController
+
   def create
     @prototype = Prototype.find(params[:prototype_id])
     current_user.likes.create(create_params)
     @like = set_like
-    @likes = Like.where(prototype_id: params[:prototype_id]).count
+    set_likes
   end
 
   def destroy
     @prototype = Prototype.find(params[:prototype_id])
     Like.destroy(set_like)
-    @likes = Like.where(prototype_id: params[:prototype_id]).count
+    set_likes
   end
 
   private
@@ -19,6 +20,10 @@ class LikesController < ApplicationController
 
   def set_like
     Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+  def set_likes
+    @likes = Like.where(prototype_id: params[:prototype_id]).count
   end
 
 end
