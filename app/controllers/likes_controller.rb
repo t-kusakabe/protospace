66a0880ff9ec065523a2276c850b4e3dfd79f8f0
t-kusakabe <ttmac0704@gmail.com>
2016-03-08 17:24:15 +0900

    likes_controllerにdestroyアクション追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,18 +2,23 @@ class LikesController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @like = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
-    like = Like.where(prototype_id: params[:prototype_id], user_id: current_user.id)
-    
-    if like.present?
-      Like.destroy(like)
-    else
-      current_user.likes.create(create_params)
-    end
+    @like = set_like
+    current_user.likes.create(create_params)
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    Like.destroy(set_like)
   end
 
   private
   def create_params
     params.permit(:prototype_id)
   end
+
+  def set_like
+    Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
 end
