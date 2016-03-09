ebe803eec9f4b98947be9ab228c630da55001c70
t-kusakabe <ttmac0704@gmail.com>
2016-03-09 12:19:58 +0900

    counter_cache導入

--- app/controllers/populars_controller.rb
+++ app/controllers/populars_controller.rb
@@ -2,11 +2,6 @@ class PopularsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    prototype_likes = Like.group(:prototype_id).order('count_prototype_id DESC').count(:prototype_id).keys
-    prototype_ids = Prototype.where.not(id: prototype_likes).group(:id).count(:id).keys
-    prototype_ids.each do |prototype|
-      prototype_likes << prototype
-    end
-    @prototypes = prototype_likes.map{ |id| Prototype.find(id) }
+    @prototypes = Prototype.order('like_count DESC')
   end
 end
