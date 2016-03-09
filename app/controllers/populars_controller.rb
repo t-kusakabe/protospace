64671c95390b7fa39e6eda2daa253e6b2c9047b4
t-kusakabe <ttmac0704@gmail.com>
2016-03-09 11:20:04 +0900

    人気順に並び替え実装

--- app/controllers/populars_controller.rb
+++ app/controllers/populars_controller.rb
@@ -2,6 +2,11 @@ class PopularsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @prototypes = Prototype.all
+    prototype_likes = Like.group(:prototype_id).order('count_prototype_id DESC').count(:prototype_id).keys
+    prototype_ids = Prototype.where.not(id: prototype_likes).group(:id).count(:id).keys
+    prototype_ids.each do |prototype|
+      prototype_likes << prototype
+    end
+    @prototypes = prototype_likes.map{ |id| Prototype.find(id) }
   end
 end
