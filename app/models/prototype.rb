effdcdd568e413f1ca989c6da035ace58de48555
t-kusakabe <ttmac0704@gmail.com>
2016-03-14 19:45:24 +0900

    thumbnailの削除
    親のプロトタイプを削除した時に子のサムネイルも削除

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :delete_all
-  has_many :thumbnails
+  has_many :thumbnails, dependent: :delete_all
   has_many :likes
   accepts_nested_attributes_for :thumbnails, reject_if: proc { |attributes| attributes[:thumbnail].blank? }
 
