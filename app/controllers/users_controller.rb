a7820089313edbaa57f690b04432fb9a777eb691
t-kusakabe <ttmac0704@gmail.com>
2016-03-16 00:46:36 +0900

    My Page修正
    マイページのバグを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @prototype_num = Prototype.where(user_id: current_user.id)
-    @prototypes = @prototype_num.page(params[:page]).per(5)
+    @prototypes = Prototype.where(user_id: params[:id]).page(params[:page]).per(5)
+    @user = User.find(params[:id])
   end
 end
