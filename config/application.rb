74b8ae066364abc695460217bb882bd62438ed78
t-kusakabe <ttmac0704@gmail.com>
2016-05-30 11:13:13 +0900

    change_timezone
    タイムゾーンを東京に

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,8 @@ Bundler.require(*Rails.groups)
 module Protospace
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    # 日本時刻の設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
