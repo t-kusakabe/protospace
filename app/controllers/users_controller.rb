36518a95af500fc0758e1552ad052bf51897d39e
t-kusakabe <ttmac0704@gmail.com>
2016-09-28 13:43:22 +0900

    なんかいっぱい

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @prototypes = Prototype.where(user_id: params[:id]).page(params[:page]).per(5).includes(:user, :tags)
+    @prototypes = Prototype.where(user_id: params[:id]).page(params[:page]).per(5).includes(:user, :tags, :tags, :tag_taggings)
     @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'prototypes#index'
-  resources :newests, only: [:index]
-  resources :users, only: [:show]
+  resources :newests, only: :index
+  resources :users, only: :show
   resources :tags, only: [:index, :show]
-  resources :prototypes, except: [:index] do
-    resources :comments, only: [:create]
+  resources :prototypes, except: :index do
+    resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
   end
 end
